6382dd4ab01b52d422c8fab43741b2d0e6723830
Tyr Fahey <60363999+TyrFahey@users.noreply.github.com>
2020-03-02 14:03:45 -0800

    Create conditional-practice-problem-1.rb
    Conditional practice problem from the Ada JumpStart curriculum lesson 10.

---
+++ control-flow-diagrams/conditional-practice-problem-1.rb
@@ -0,0 +1,11 @@
+# Conditional practice problem #1 from the Ada JumpStart conditionals worksheet.
+
+# Assuming that the x and y variable have been set by user input prior to code execution:
+
+if x == y
+  print 'equal'
+ elsif x < y
+  print 'less'
+ else
+  print 'greater'
+end
